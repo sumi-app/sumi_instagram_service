6f7127b72f5497338ee2e3be79769a5871bd9e80
Stanislav Ilin <youngfrezyx@gmail.com>
2021-08-21 02:36:14 +0500

    Add instagram message sender with selena

--- api.py
+++ api.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import os
+from message_sender.msg_sender import send_message_to_account
 
 app = Eve()
 
@@ -35,6 +36,9 @@ def get_arg_from_request(req, arg_name: str):
 @app.route('/write')
 def write_to_user():
     ig_login = get_arg_from_request(request, 'login')
+
+    send_message_to_account(ig_login)
+
     response = app.response_class(
         response=json.dumps(ig_login, ensure_ascii=False, indent=4),
         status=200,
---
+++ message_sender/msg_sender.py
@@ -0,0 +1,65 @@
+from time import sleep
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import json
+
+
+def send_message_to_account(login):
+    chrome_options = Options()
+    # Add for opening screen
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument('log-level=2')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), 0, chrome_options)
+
+    driver.get("https://www.instagram.com/")
+
+    sleep(2)
+
+    login_field = driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
+    login_field.send_keys('uralskycry')
+
+    pwd_field = driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+    pwd_field.send_keys('FROMTHEURAL232323')
+
+    driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button").click()
+
+    sleep(10)
+
+    driver.get(f"https://www.instagram.com/{login}")
+
+    sleep(5)
+
+    try:
+        follow_button = driver.find_element_by_xpath(
+            "/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button")
+        if follow_button and follow_button.text == 'Подписаться':
+            follow_button.click()
+    except:
+        print('Follow exception')
+
+    sleep(5)
+
+    message_open_button = driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div[1]/button")
+    message_open_button.click()
+
+    sleep(5)
+
+    try:
+        close_modal_button = driver.find_element_by_xpath("/html/body/div[6]/div/div/div/div[3]/button[2]")
+        if close_modal_button:
+            close_modal_button.click()
+    except:
+        print('Close banner exception')
+
+    text_field = driver.find_element_by_xpath("/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea")
+    text_field.send_keys("Я бот ла ла ла ла пишу с сервера SUMI")
+
+    send_msg_button = driver.find_element_by_xpath("/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button")
+    send_msg_button.click()
