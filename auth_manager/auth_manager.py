12bf9395712b446a82fed2f48020d321939efb04
Stanislav Ilin <youngfrezyx@gmail.com>
2021-08-21 15:56:43 +0500

    Fix auth module

--- auth_manager/auth_manager.py
+++ auth_manager/auth_manager.py
@@ -3,13 +3,15 @@ import os
 
 
 def auth_with_login_and_pass(driver):
-    driver.get("https://www.instagram.com/")
-    sleep(2)
-    fields = driver.find_elements_by_class_name("_2hvTZ")
-    login_field = fields[0]
+    driver.get("https://www.instagram.com/accounts/login/")
+    sleep(10)
+    login_field = driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[1]/div/label/input")
     login_field.send_keys(os.getenv('INSTA_LOGIN'))
-    pwd_field = fields[1]
+
+    pwd_field = driver.find_element_by_xpath(
+        "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[2]/div/label/input")
     pwd_field.send_keys(os.getenv('INSTA_PASS'))
     driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button").click()
+        "/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[3]/button").click()
     sleep(3)
