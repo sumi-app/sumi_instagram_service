7f0729a802944051fd285e1fd29e0fea14f28274
Stanislav Ilin <youngfrezyx@gmail.com>
2021-08-21 16:27:52 +0500

    Add caching chromedriver

--- message_sender/msg_sender.py
+++ message_sender/msg_sender.py
@@ -6,16 +6,39 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import json
-
+from selenium.webdriver.chrome.webdriver import WebDriver
 from auth_manager.auth_manager import auth_with_login_and_pass
 
 
+
+def get_chrome_driver() -> WebDriver:
+    options = webdriver.ChromeOptions()
+    chrome_driver_binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver: WebDriver = None
+
+    # options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
+    if chrome_driver_binary_location == None:  # running in test env
+        driver = webdriver.Chrome(
+            ChromeDriverManager().install(), chrome_options=options)
+    else:  # running in server env
+        options.binary_location = chrome_driver_binary_location
+
+        driver = webdriver.Chrome(executable_path=os.environ.get(
+            "CHROMEDRIVER_PATH"), chrome_options=options)
+
+    return driver
+
 def send_message_to_account(login, bot_msg):
-    chrome_options = Options()
-    # Add for opening screen
-    # chrome_options.add_argument("--headless")
-    chrome_options.add_argument('log-level=2')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), 0, chrome_options)
+    # chrome_options = Options()
+    # # Add for opening screen
+    # # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument('log-level=2')
+    # driver = webdriver.Chrome(ChromeDriverManager().install(), 0, chrome_options)
+
+    driver = get_chrome_driver()
 
     auth_with_login_and_pass(driver)
 
